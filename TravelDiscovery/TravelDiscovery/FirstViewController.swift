3dfb6aa7d13bb8379a365308cacc95573cf15e67
Jan <kaiserj@cip.ifi.lmu.de>
2017-11-22 00:14:31 +0100

    app template, gitignore update
    created app template, including an extra packet for login
    
    updated gitignore to include mac related files

---
+++ TravelDiscovery/TravelDiscovery/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TravelDiscovery
+//
+//  Created by Jan on 22.11.17.
+//  Copyright © 2017 Jan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TravelDiscovery/TravelDiscovery/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TravelDiscovery
+//
+//  Created by Jan on 22.11.17.
+//  Copyright © 2017 Jan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
