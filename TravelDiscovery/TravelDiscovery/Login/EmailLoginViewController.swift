65cf272d9de7649ed933f343e25c1bd436f349f8
Jan <kaiserj@cip.ifi.lmu.de>
2017-11-22 23:39:56 +0100

    added podfile, started working on email based login
    added firebase authentication to podfile
    created storyboard for email based login
    added alerts to email based login
    
    no functionality related to firebase has so far been added.

---
+++ TravelDiscovery/TravelDiscovery/Login/EmailLoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  EmailLoginViewController.swift
+//  TravelDiscovery
+//
+//  Created by Jan on 22.11.17.
+//  Copyright © 2017 Jan. All rights reserved.
+//
+
+import UIKit
+
+class EmailLoginViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
