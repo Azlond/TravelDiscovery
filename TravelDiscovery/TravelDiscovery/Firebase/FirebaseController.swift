5ddd47322a4d13fe5cdb0ed04a17b6b06668e047
Jan <github@sintho.com>
2017-12-09 08:40:11 -0800

    App no longer crashes when scratching countries with '.' in their name
    using regex to replace '.' with DOT for serverside storage

--- TravelDiscovery/TravelDiscovery/Firebase/FirebaseController.swift
+++ TravelDiscovery/TravelDiscovery/Firebase/FirebaseController.swift
@@ -14,7 +14,18 @@ class FirebaseController {
     
     public static func saveToFirebase() {
         if let user = Auth.auth().currentUser {
-            FirebaseData.ref.child("users").child(user.uid).setValue(["visitedCountries": FirebaseData.visitedCountries])
+            if (FirebaseData.ref == nil) {
+                // initialize database
+                FirebaseData.ref = Database.database().reference()
+            }
+            var vC : Dictionary<String, Bool> = [:]
+            let regex = try! NSRegularExpression(pattern: "\\.")
+            for country in FirebaseData.visitedCountries {
+                let key = regex.stringByReplacingMatches(in: country.key, options: [], range: NSRange(0..<country.key.utf16.count), withTemplate: "DOT")
+                vC[key] = true
+            }
+            print(vC)
+            FirebaseData.ref.child("users").child(user.uid).setValue(["visitedCountries": vC])
         }
     }
     
@@ -26,7 +37,14 @@ class FirebaseController {
             }
             FirebaseData.ref.child("users").child(user.uid).observeSingleEvent(of: .value, with: { (snapshot) in
                 let value = snapshot.value as? NSDictionary
-                FirebaseData.visitedCountries = value?["visitedCountries"] as? Dictionary<String, Bool> ?? [:]
+                var vC : Dictionary<String, Bool> = [:]
+                let fbD = value?["visitedCountries"] as? Dictionary<String, Bool> ?? [:]
+                let regex = try! NSRegularExpression(pattern: "DOT")
+                for country in fbD {
+                    let key = regex.stringByReplacingMatches(in: country.key, options: [], range: NSRange(0..<country.key.utf16.count), withTemplate: ".")
+                    vC[key] = true
+                }
+                FirebaseData.visitedCountries = vC
                 NotificationCenter.default.post(name: Notification.Name("updateMap"), object: nil)
             }) { (error) in
                 print(error.localizedDescription)
