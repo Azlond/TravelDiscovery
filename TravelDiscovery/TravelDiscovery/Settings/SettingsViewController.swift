960146e0bb44eea7b6f8df673588e44c067cad1b
Jan <github@sintho.com>
2017-12-15 14:31:43 -0800

    implemented stepcounter functionality. Placed in settings, needs to be moved to travels once finished

--- TravelDiscovery/TravelDiscovery/Settings/SettingsViewController.swift
+++ TravelDiscovery/TravelDiscovery/Settings/SettingsViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 import FirebaseAuth
 import Eureka
+import HealthKit
 
 class SettingsViewController: FormViewController {
 
+    @IBOutlet weak var stepLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let userSettings = UserDefaults.standard
@@ -80,7 +83,58 @@ class SettingsViewController: FormViewController {
             selector: #selector(updateSettings),
             name: Notification.Name("updateSettings"),
             object: nil)
+        // Do any additional setup after loading the view.
+        stepCounter()
     }
+    
+    func stepCounter() {
+        guard let healthStore: HKHealthStore? = {
+            if HKHealthStore.isHealthDataAvailable() {
+                return HKHealthStore()
+            } else {
+                return nil
+            }
+            }() else {
+                return
+        }
+        
+        let stepsCount = HKQuantityType.quantityType(forIdentifier: .stepCount)
+        
+        let dataTypesToRead : Set<HKObjectType> = [stepsCount!]
+        
+        healthStore?.requestAuthorization(toShare: nil, read: dataTypesToRead) { (success, error) in
+            if let error = error {
+                print("Failed authorization = \(error.localizedDescription)")
+            }
+        }
+        
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+
+        
+        let stepsSampleQuery = HKStatisticsQuery(quantityType: stepsCount!, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
+            var resultCount = 0.0
+            
+            guard let result = result else {
+                print("Failed to fetch steps = \(error?.localizedDescription ?? "N/A")")
+                return
+            }
+            
+            if let sum = result.sumQuantity() {
+                resultCount = sum.doubleValue(for: HKUnit.count())
+            }
+            print(resultCount)
+            /*DispatchQueue.main.async(execute: {
+                self.stepLabel.text = String(Int(resultCount))
+            })*/
+        }
+        
+        // Don't forget to execute the Query!
+        healthStore?.execute(stepsSampleQuery)
+    }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
