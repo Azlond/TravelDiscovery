63010c74e7708bb27c010120ab737f4537366e2e
Jan <github@sintho.com>
2017-12-16 16:11:27 -0800

    settings are now stored locally

--- TravelDiscovery/TravelDiscovery/Settings/SettingsViewController.swift
+++ TravelDiscovery/TravelDiscovery/Settings/SettingsViewController.swift
@@ -14,7 +14,8 @@ class SettingsViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let userSettings = UserDefaults.standard
+
         form +++ Section("Accounts Details")
             <<< EmailRow(){ row in
                 row.title = "E-Mail Address"
@@ -35,17 +36,32 @@ class SettingsViewController: FormViewController {
             <<< NameRow(){ row in
                 row.title = "Your Name"
                 row.placeholder = "John Doe"
+                row.value = userSettings.string(forKey: "username")
+                row.onChange({row in
+                    guard let value = row.value else {
+                        return
+                    }
+                    userSettings.set(value, forKey: "username")
+                })
             }
             <<< SwitchRow { row in
-                row.title = "Standard Visibility: Public"
-                row.value = false
+                row.value = userSettings.bool(forKey: "visibility") /*If not existing, returns false*/
+                row.title = row.value! ? "Standard Visibility: Public" : "Standard Visibility: Private"
+                row.onChange({row in
+                    row.title = row.value! ? "Standard Visibility: Public" : "Standard Visibility: Private"
+                    row.updateCell()
+                    userSettings.set(row.value, forKey: "visibility")
+                })
             }
             <<< SliderRow { row in
                 row.title = "Feed Range (km)"
                 row.minimumValue = 1
                 row.maximumValue = 50
                 row.steps = UInt(row.maximumValue - row.minimumValue)
-                row.value = row.minimumValue
+                row.value = userSettings.integer(forKey: "feedRange") != 0 ? userSettings.float(forKey: "feedRange") : 1.0
+                row.onChange({row in
+                    userSettings.set(row.value, forKey: "feedRange")
+                })
         }
     }
 
@@ -66,7 +82,11 @@ class SettingsViewController: FormViewController {
         }
         self.view.window!.rootViewController?.dismiss(animated: true, completion: nil)
     }
-    
+    /**
+     * Ask for confirmation before deletion
+     * removes all user data and removes the account.
+     * Cannot be undone
+     */
     func deleteAccount(cell: ButtonCellOf<String>, row: ButtonRow) {
         let deleteConfirmationAlert = UIAlertController(title: "WARNING: Account Deletion", message: "Are you sure you wish to delete your account? This cannot be undone!", preferredStyle: .alert)
         deleteConfirmationAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction!) in
