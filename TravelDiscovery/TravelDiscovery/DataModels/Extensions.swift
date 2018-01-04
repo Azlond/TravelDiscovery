7ce9a2e7c84de67282c2f59786a7dde97bf54d68
Jan <github@sintho.com>
2018-01-04 15:04:01 -0800

    added done button on keyboard to all textfields

---
+++ TravelDiscovery/TravelDiscovery/DataModels/Extensions.swift
@@ -0,0 +1,76 @@
+//
+//  Extensions.swift
+//  TravelDiscovery
+//
+//  Created by Jan Kaiser on 04.01.18.
+//  Copyright © 2018 Jan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/*Adds a preference for adding a done button to the keyboard when using UITextView*/
+extension UITextView {
+    /*Expose preference to storyboard*/
+    @IBInspectable var doneAccessory: Bool {
+        get{
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+    /*Add toolbar with done button*/
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    /*Done action*/
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
+}
+
+/*Adds a preference for adding a done button to the keyboard when using UITextView*/
+extension UITextField {
+    /*Expose preference to storyboard*/
+    @IBInspectable var doneAccessory: Bool{
+        get{
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+    /*Add toolbar with done button*/
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    /*Done action*/
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
+}
