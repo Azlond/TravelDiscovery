6859db5c09d177070783a5d57a7b7ffcc103a2b1
Laura <lauri_m@gmx.de>
2018-01-12 15:46:32 +0100

    Sart Screen Animation Update

--- TravelDiscovery/TravelDiscovery/Login/LoginViewController.swift
+++ TravelDiscovery/TravelDiscovery/Login/LoginViewController.swift
@@ -14,6 +14,7 @@ import FirebaseAuth
 class LoginViewController: UIViewController {
     
     
+    @IBOutlet weak var logo: UIImageView!
     @IBOutlet weak var appName: UILabel!
     @IBOutlet weak var subText: UILabel!
     @IBOutlet weak var loginOptionsView: UIView!
@@ -28,32 +29,33 @@ class LoginViewController: UIViewController {
         subText.alpha = 1.0
         loginOptionsView.alpha = 0
         
-        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
             self.subText.alpha = 0
-                      
+            
         }, completion: {(completed) in
             UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
-                self.loginOptionsView.center.y -= 30
+                self.appName.center.y -= 50
+                self.logo.center.y -= 50
+                self.loginOptionsView.center.y -= 10
                 self.loginOptionsView.alpha = 1
-        })
-            
+            })
         })
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
