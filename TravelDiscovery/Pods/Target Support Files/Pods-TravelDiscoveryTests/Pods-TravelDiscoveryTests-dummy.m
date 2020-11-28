a9a09eb09acba6941f8b8cab1fe8902a8602a357
Jan <jan@Jans-Mac.local>
2020-11-28 15:35:36 -0800

    fix: removed pods from git tracking

--- TravelDiscovery/Pods/Target Support Files/Pods-TravelDiscoveryTests/Pods-TravelDiscoveryTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TravelDiscoveryTests : NSObject
-@end
-@implementation PodsDummy_Pods_TravelDiscoveryTests
-@end
--- TravelDiscovery/Pods/Target Support Files/ScratchCard/ScratchCard-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ScratchCard : NSObject
-@end
-@implementation PodsDummy_ScratchCard
-@end
